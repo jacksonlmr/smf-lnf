54221441479c6110fe93d04735fbd196a22ed79b
jacksonlmr <38480226+jacksonlmr@users.noreply.github.com>
2026-07-01 20:15:39 +0000

    Added generate_img_zip to generate a zip file from a list of PIL images
    Added so that a zip file containing all the invalid images can be downloaded by the user, allowing for verification of model results.

--- src/services/data_service.py
+++ src/services/data_service.py
@@ -1,6 +1,9 @@
 from typing import List
+import zipfile
+import io
 
 import pandas as pd
+from PIL.ImageFile import ImageFile
 
 from src.models.llm_schemas import FoundItem
 
@@ -15,8 +18,9 @@ COLUMNS = [
 
 
 def to_dataframe(items: List[FoundItem | None]) -> pd.DataFrame:
-    rows: list[dict[str, str | bool]] = [
+    rows: list[dict[str, str | bool | int | None]] = [
         {
+            "Is Valid Form": item.is_valid_form,
             "Item Number": item.item_number,
             "Description": item.description,
             "Category": item.category,
@@ -28,3 +32,21 @@ def to_dataframe(items: List[FoundItem | None]) -> pd.DataFrame:
         if item is not None
     ]
     return pd.DataFrame(rows) if rows else pd.DataFrame(columns=COLUMNS)
+
+def generate_img_zip(imgs: List[ImageFile]):
+    """
+    Generates a zip file from a list of PIL images
+    """
+    zip_buffer = io.BytesIO()
+
+    with zipfile.ZipFile(zip_buffer, "w", zipfile.ZIP_DEFLATED) as zip_file:
+        for i, pil_image in enumerate(imgs):
+            img_buffer = io.BytesIO()
+            pil_image.save(img_buffer, format="JPEG")
+            img_bytes = img_buffer.getvalue()
+
+            zip_file.writestr(f"invalid_img_{i}.jpg", img_bytes)
+
+    zip_buffer.seek(0)
+    return zip_buffer
+
